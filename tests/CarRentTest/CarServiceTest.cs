2c91c8b6204bb34e8e571b9b8c9784af77a64b66
Michal Boniakowski <michal.boniakowski11@gmail.com>
2021-06-10 12:20:26 +0200

    adding auto fixture to tests

--- tests/CarRentTest/CarServiceTest.cs
+++ tests/CarRentTest/CarServiceTest.cs
@@ -1,12 +1,17 @@
+using AutoFixture;
 using AutoMapper;
 using CarRent.Data;
+using CarRent.Dtos;
 using CarRent.Models;
 using CarRent.Profiles;
 using CarRent.Services;
+using FluentAssertions;
 using Moq;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
+using System.Linq;
 
 namespace CarRentTest
 {
@@ -16,6 +21,7 @@ namespace CarRentTest
         private readonly Mock<IRentalCarRepo> _repoMock = new Mock<IRentalCarRepo>();
         private readonly Mock<ILoggingService> _logger = new Mock<ILoggingService>();
         private readonly IMapper _mapper;
+        private readonly IFixture _fixture = new Fixture(); 
 
 
         public CarServiceTest()
@@ -87,6 +93,27 @@ namespace CarRentTest
 
             //assert
             _logger.Verify(x => x.LoggMessage($"Car with id: {id} was not found."), Times.Once);           
+        }
+
+        [Fact]
+        public async Task GetCars_Should_ReturnCars()
+        {
+            //arrange
+            ICollection<RentalCar> ls = new List<RentalCar>();
+
+            _fixture.RepeatCount = 10;
+            _fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            _fixture.AddManyTo<RentalCar>(ls);
+
+            _repoMock.Setup(x => x.GetCars(It.IsAny<Int32>(), It.IsAny<Int32>())).ReturnsAsync(ls);
+
+            //act
+            var cars = await _testService.GetCars(1, 20);
+            var ls2 = _mapper.Map<IEnumerable<ReadRentalCarDto>>(ls);
+
+            //assert
+            cars.Should().BeEquivalentTo(ls2);
         }
     }
 }
